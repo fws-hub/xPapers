d38aae047960adfd1e6c2884cffa537b5657089a
zby <zzbbyy@gmail.com>
2010-11-25 17:46:48 +0000

    generating yaml data for merging pods

--- bin/dev/merge_pod.pl
+++ bin/dev/merge_pod.pl
@@ -6,6 +6,8 @@ use HTML::Mason;
 
 use File::Slurp qw/ slurp read_file/;
 use File::Find::Rule;
+use YAML qw/LoadFile DumpFile/;
+use Hash::Merge 'merge';
 
 my @files = File::Find::Rule->file()
     ->name( '*.pm' )
@@ -15,7 +17,7 @@ my $outbuf;
 my $interp = HTML::Mason::Interp->new( comp_root => '/home/xpapers/bin/dev', out_method => \$outbuf );
 for my $file( @files ){
     print "fixing $file\n";
-    my $pod_gen;
+    my %pod_gen;
     my $package;
     for my $line ( read_file( $file ) ){
         if( $line =~ /package\s*((\w|::)+)\b/ ){
@@ -43,74 +45,59 @@ for my $file( @files ){
     }
     my %methods = map { $_ => 1 } $pc->naked;
    
-    $pod_gen
-    my $output = '';
-    $output = qq{
-__POD__
-
-=head1 NAME
-
-$package
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-};
+    $pod_gen{NAME} = $package;
     my $ismoose;
+    my $field;
     if( $package->can( 'meta' ) ){
         my @isa = eval '@' . $package . '::ISA';
         if(@isa){
-            $output .= 'Inherits from: ';
-            $output .= join ', ', map "L<$_>", @isa;
-            $output .= "\n\n";
+            $pod_gen{DESCRIPTION} .= 'Inherits from: ';
+            $pod_gen{DESCRIPTION} .= join ', ', map "L<$_>", @isa;
+            $pod_gen{DESCRIPTION} .= "\n\n";
         }
         my $meta = $package->meta;
         if( $meta->isa( 'Rose::DB::Object::Metadata' ) ){
-            $output .= "Table: " . $meta->table . "\n\n";
-            $output .= "=head1 FIELDS\n\n";
+            $pod_gen{DESCRIPTION} .= "Table: " . $meta->table . "\n\n";
             for my $col ( sort $meta->columns ){
                 my $type = $col->type;
                 $type = uc $type if $type eq 'set' || $type eq 'array';
-                $output .= '=head2 ' . $col->name . " ($type):\n\n";
+                $pod_gen{FIELDS}{$col->name} = "($type):";
                 delete $methods{$col->name};
             }
         }
         elsif( $meta->can( 'get_all_attributes' ) ){
-            $output .= "=head1 ATTRIBUTES\n\n";
             for my $attr ( sort $meta->get_all_attributes ) {
-                $output .= '=head2 ' . $attr->name . "\n\n";
+                $pod_gen{ATTRIBUTES}{$attr->name} = '';
                 delete $methods{$attr->name};
             }
             $ismoose = 1;
         }
-        $output .= "=head1 METHODS\n\n";
+        $field = 'METHODS';
     }
     elsif( $package->isa( 'Rose::DB::Object::Manager' ) ){
-        $output .= "=head1 METHODS\n\n";
+        $field = 'METHODS';
     }
     else{
-        $output .= "=head1 SUBROUTINES\n\n";
+        $field = 'SUBROUTINES';
     }
 
     for my $method ( sort keys %methods ){
-        $output .= "=head2 $method\n\n";
+        $pod_gen{$field}{$method} = 1;
     }
 
-    $output .= "=head1 DIAGNOSTICS\n\n";
-    $output .= "=head1 AUTHORS\n\n";
     if( $ismoose ){
-        $output .= "Zbigniew Lukasiak\n";
-        $output .= "with contibutions David Bourget\n\n";
+        $pod_gen{AUTHORS} = "Zbigniew Lukasiak\nwith contibutions David Bourget\n\n";
     }
     else{
-        $output .= "David Bourget\n";
-        $output .= "with contibutions from Zbigniew Lukasiak\n\n";
+        $pod_gen{AUTHORS} = "David Bourget\nwith contibutions from Zbigniew Lukasiak\n\n";
     }
-    $output .= "head1 COPYRIGHT AND LICENSE\n\n";
-    $output .= "See accompanying README file for licensing information.\n\n";
-
-
-    print $output;
+    my $pod_file = $file . '.yaml_pod';
+    my $pod_old = {};
+    if( -f $pod_file ){
+        $pod_old = LoadFile( $pod_file );
+    }
+    my $pod_merged = merge( \%pod_gen, $pod_old );
+    DumpFile( $pod_file, $pod_merged );
 }
+
 
