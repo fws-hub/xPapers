06c94f7a3d4cebff43615619f916e6665b2d21a6
zby <zzbbyy@gmail.com>
2010-11-26 14:25:35 +0000

    applying the pod generated from the temporary files

---
+++ bin/dev/apply_gen_pod.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+
+use Pod::Coverage;
+use HTML::Mason;
+
+use File::Slurp qw/ slurp write_file/;
+use File::Find::Rule;
+use YAML qw/LoadFile DumpFile/;
+use Hash::Merge 'merge';
+
+my @files = File::Find::Rule->file()
+    ->name( '*.pm' )
+    ->in( 'lib/xPapers' );
+
+my $outbuf;
+my $interp = HTML::Mason::Interp->new( comp_root => '/home/xpapers/bin/dev', out_method => \$outbuf );
+for my $file( @files ){
+    print "fixing $file\n";
+    my $pod_file = $file . '.yaml_pod';
+    next if ! -f $pod_file;
+    my $pod = LoadFile( $pod_file );
+    $outbuf = '';
+    $interp->exec( '/pod_template.mason', %$pod );
+    write_file( $file, {append => 1}, $outbuf ) ;
+}
+
+
+
