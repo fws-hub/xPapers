1e2ef049b21b01d312863f839097b2970261006d
David Bourget <david.bourget@gmail.com>
2010-12-02 15:51:26 +0000

    bad doc

--- lib/xPapers/Object/Base.pm
+++ lib/xPapers/Object/Base.pm
@@ -368,3 +368,33 @@ LICENCING_STUFF
 
 
 
+__POD__
+
+=head1 NAME
+
+
+
+=head1 VERSION
+
+...
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 ATTRIBUTES and METHODS
+
+
+=head1 DIAGNOSTICS
+
+...
+
+LICENCING_STUFF
+
+
+
+
