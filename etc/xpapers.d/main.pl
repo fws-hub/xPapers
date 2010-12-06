0ff73d9653a10e08edeef46dee01a261192c7459
David Bourget <david.bourget@gmail.com>
2010-12-06 16:17:02 +0000

    removed useless config options

--- etc/xpapers.d/main.pl
+++ etc/xpapers.d/main.pl
@@ -6,7 +6,7 @@
     database => 'db',
     host     => 'localhost;enable_mysql_utf8=1',
     username => 'xpapers',
-    password => 'k1w1br0k4r',
+    password => 'CHANGEME',
     server_time_zone => 'GMT',
 );
 
@@ -39,7 +39,7 @@ $SMTPHOST = "localhost";
 $TEST_MODE = 0;
 
 # For the password crypt function. Any string here. Maybe 5-10 chars long.
-$PASSWD_SALT = "0-20~~~~~~~~";
+$PASSWD_SALT = "CHANGEME";
 
 # Domains whose links are considered unlikely to break. You should add any domain from which you have many links here.
 # These will be checked less systematically by the link checker, saving a lot of time.
@@ -81,30 +81,24 @@ $TZ_OFFSET_STR = formatTZ($TZ_OFFSET);
 
 # Your Facebook App details
 %FACEBOOK = (
-    APP_ID => "c77437fbbdab367f5d888b26b140a337",
-    API_KEY => "148977641796357"
+    APP_ID => "CHANGEME",
+    API_KEY => "CHANGEME"
 );
 
 # Your ReCAPTCHA credentials
-$RECAP_KEY = '6LdDNAMAAAAAAOJ1_JBkyr7g_zTzNcmvG8Y18w9r';
-$RECAP_PUBKEY = '6LdDNAMAAAAAAB2nJfM_g7jPAyYR5xmReCTJVvYW';
+$RECAP_KEY = 'CHANGEME';
+$RECAP_PUBKEY = 'CHANGEME';
 
 # Your Amazon Affiliate credentials
 %AMAZON = (
     data_dir => "$PATHS{LOCAL_BASE}/var/data/amazon",
-    key    => '1CYYSXRPEAM0Q99H1WR2', 
-    secret => '5Jl9oCqqQPt3tv9C1xIfxJ2bF2q48i9FPqZll8Jt',
+    key    => 'CHANGEME', 
+    secret => 'CHANGEME',
     associate_tag => {
         uk => 'philp-21', # change that too 
         us => 'philp-20',
         ca => 'philp07-20',
     }
-);
-
-%ABEBOOKS = (
-    clientkey => '060d105b-6d78-4f95-b520-da98c97def75',
-    data_dir  => "$PATHS{LOCAL_BASE}/var/data/abebooks",
-    locales   => { us => 'USD', uk => 'GBP', ca => 'CAD', au => 'AUD' },
 );
 
 
@@ -147,22 +141,7 @@ $DEFAULT_SITE_NAME = 'philpapers';
 # Where to point for your license for OAI-PMH data
 $OAI_METADATA_RIGHTS_REF = $DEFAULT_SITE->{server} . '/help/terms.html';
 
-#
-# Special requests
-#
-
-%SPECIAL_REQUESTS = (
-
-    most_cited_sci => {where=>"philosophy=0 and duplicate=0",order=>'citations desc, id',limit=>'100',desc=>"100 most cited works by scientists[ in MindPapers according to Google Scholar]", prefix=>'citations' },
-     most_cited_phil => {where=>"philosophy=1 and duplicate=0",order=>'citations desc, id',limit=>'100',desc=>"100 most cited works by philosophers[ in MindPapers according to Google Scholar]", prefix=>'citations' },
-
-    most_cited => {where=>"duplicate=0",order=>'citations desc, id',limit=>'100',desc=>"100 most cited works[ in MindPapers according to Google Scholar]", prefix=>'citations'},
-
-    most_viewed => {where=>'duplicate=0',order=>'viewings desc, authors, date',limit=>'100',desc=>"100 most viewed works",prefix=>'viewings'}
-);
-
-
-# Our colour scheme
+# Your colour scheme
 $C1 = '666666';
 $C2 = '10A010'; 
 $C3 = '133d9f';
