78b5f6605b051ca704829932737294495f9ab389
Colin Sauze <cos@aber.ac.uk>
2011-10-26 14:55:26 +0100

    copied from assets

---
+++ sites/fwshub/mason/bbs/rss_threads.pl
@@ -0,0 +1,41 @@
+<%perl>
+my $x = new XML::RSS (version => '1.0');
+$x->channel(
+    title => $ARGS{title},
+    link => $ARGS{link},
+    date => DateTime->now->iso8601(), 
+    syn => {
+        updatePeriod => 'daily',
+        updateFrequency => 1,
+        updateBase => '1901-01-01T00:00+00:00',
+    },
+    dc => {
+        publisher=>$s->{niceName},
+    },
+    taxo => [
+    ]
+);
+
+for my $t (@{$ARGS{__threads__}}) {
+    next unless $t;
+    my $fp = $t->firstPost;
+    next unless $fp;
+    my ($desc,$b) = $fp->body; #$rend->wordSplit($fp->body,100);
+    my $subject = $fp->thread->forum->cId ? $fp->thread->forum->category->name : undef;
+    $x->add_item(
+       title=>$fp->user->fullname . ": " . $fp->subject,
+       link=>$rend->postURL($fp),
+       description=>$desc . ($b ? "..." : ""),
+       dc => {
+            creator => $fp->user->fullname,
+            subject=> $subject,
+            date=> $fp->created->ymd,
+            identifier=>"$DEFAULT_SITE->{server}/post/" . $fp->id
+       }
+    );
+}
+
+print $x->as_string;
+</%perl>
+
+
