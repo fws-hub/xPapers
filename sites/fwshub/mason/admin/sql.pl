063185b41b52bd96c12e71882d6e61207d488478
Colin Sauze <cos@aber.ac.uk>
2011-10-27 15:19:27 +0100

    removed ability to execute SQL here, major SQL injection vulnerability (see issue #19).

--- sites/fwshub/mason/admin/sql.pl
+++ sites/fwshub/mason/admin/sql.pl
@@ -7,20 +7,20 @@ foreach (@params) {
     $qq =~ s/\[\[$_\]\]/quote($ARGS{$_})/e;		
 }
 print "<p>Result of SQL statement `$qq`:<p>";
-my $res = $root->dbh->prepare($qq);
-$res->execute;
+#my $res = $root->dbh->prepare($qq);
+#$res->execute;
 my $count = 0;
 print "<table>\n";
-while (my $r = $res->fetchrow_hashref) {
-    print "<tr>"; 
-    if ($count == 0) {
-        print "<tr style='font-weight:bold'>";
-        print "<td>&nbsp</td><td>$_</td>" for keys %$r;	
-        print "</tr>\n";
-    }
-    print "<td>&nbsp;</td><td>$_</td>" for values %$r;
-    print "</td>";
-    $count++;
-}
+#while (my $r = $res->fetchrow_hashref) {
+#    print "<tr>"; 
+#    if ($count == 0) {
+#        print "<tr style='font-weight:bold'>";
+#        print "<td>&nbsp</td><td>$_</td>" for keys %$r;	
+#        print "</tr>\n";
+#    }
+#    print "<td>&nbsp;</td><td>$_</td>" for values %$r;
+#    print "</td>";
+#    $count++;
+#}
 print "</table>";
 </%perl>
